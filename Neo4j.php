99b985883769676c667d0d389dc55ffd0829b3a5
Groups <esokullu@gmail.com>
2018-01-06 21:19:45 -0800

    bugfix in regards to events

--- Neo4j.php
+++ Neo4j.php
@@ -62,14 +62,14 @@ class Neo4j implements IndexInterface, ServiceInterface
             ->addConnection('bolt', $uri) 
             ->build();
         
-        $this->kernel->on('kernel.booted_up', array($this, 'subscribeGraphsystem'));
+        $this->kernel->events()->on('kernel.booted_up', array($this, 'subscribeGraphsystem'));
     }
 
     protected function subscribeGraphsystem(): void
     {
-        $this->kernel->on('graphsystem.touched', array($this, 'index'));
-        $this->kernel->on('graphsystem.node_deleted', array($this, 'nodeDeleted'));
-        $this->kernel->on('graphsystem.edge_deleted', array($this, 'edgeDeleted'));
+        $this->kernel->events()->on('graphsystem.touched', array($this, 'index'));
+        $this->kernel->events()->on('graphsystem.node_deleted', array($this, 'nodeDeleted'));
+        $this->kernel->events()->on('graphsystem.edge_deleted', array($this, 'edgeDeleted'));
     }
 
     /**
