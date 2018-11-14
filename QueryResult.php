737a43d793e855f986ab8071884c8e02619dc58b
Groups <esokullu@gmail.com>
2018-11-13 19:25:20 -0800

    same format as index-redis

--- QueryResult.php
+++ QueryResult.php
@@ -11,6 +11,8 @@
 
 namespace Pho\Kernel\Services\Index\Adapters;
 
+use GraphAware\Bolt\Result\Type\Node;
+
 /**
  * Neo4j adapter for Kernel's QueryResult class.
  * 
@@ -28,9 +30,26 @@ class QueryResult extends \Pho\Kernel\Services\Index\QueryResult
          error_log("Neo4J QueryResult executing");
          error_log("Resuts is a: ".get_class($results));
          error_log("Resuts are: ".print_r($results->records(), true));
+         $i = 0;
         foreach($results->records() as $result) // $result would be a \GraphAware\Bolt\Result\Result 
         {
-            $this->results[] = $result->values()[0]->values();
+            $keys = $result->keys();
+            $values = $result->values();
+            foreach($values as $k=>$value) {
+                if($value instanceof Node) { // if $value->values() then it's like n
+                    $_ = $value->values();
+                    foreach($_ as $old_key=>$v) {
+                        $new_key = $keys[$k].".".$old_key;
+                        $this->results[$i][$new_key] = $v;
+                        unset($this->results[$i][$old_key]);
+                    }
+                }
+                else { // otherwise it's like n.something as Something
+                    $this->results[$i][($keys[$k])] = $value;
+                }
+                $i++;
+            }
+            
         }
         $stats = $results->summarize()->updateStatistics();
          if(!is_null($stats)) {
